eaf155d217e043e37e8fe935181846af2b44fd0d
xiaojun <biejun1992@qq.com>
2021-02-01 09:33:06 +0800

    兼容php7

--- view/default/tpl/scripts.php
+++
@@ -1,9 +0,0 @@
-<?php
-$this->minifyJS([
-  SOURCES.'/js/jquery-3.5.1.min.js',
-  SOURCES.'/js/semantic.min.js',
-  SOURCES.'/js/knockout-3.4.2.js',
-  SOURCES.'/js/md5.js',
-  SOURCES.'/js/vendor.js'
-], 'default/js/chunk-common.js', '1.0.0');
-?>
--- view/welcome/tpl/common.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-  var _CONFIG_ = { path : '<?=$this->path;?>', username : '<?=$this->account->name;?>'}
-</script>
